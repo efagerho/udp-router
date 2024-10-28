34ac9475ea41259c023c62a0fcc828ef9bd3fa8c
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2024-10-28 11:21:36 +0200

    Put different server roles in different subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,78 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+#
+# VPC
+#
 
-  name = "udp-router-loadtest"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a"]
-  public_subnets  = ["10.0.0.0/24"]
-  private_subnets  = ["10.0.1.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Terraform = "true"
-    Environment = "ed-testing"
+    Name = "udp-router-loadtest"
   }
+}
+
+resource "aws_subnet" "client" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "udp-router-loadtest-client"
+  }
+}
+
+resource "aws_subnet" "router" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "udp-router-loadtest-router"
+  }
+}
+
+resource "aws_subnet" "server" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "udp-router-loadtest-server"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "udp-router-loadtest-igw"
+  }
+}
+
+resource "aws_route_table" "igw" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.main.id
+ }
+
+ tags = {
+   Name = "udp-router-loadtest-igw"
+ }
+}
+
+resource "aws_route_table_association" "client" {
+ subnet_id      = aws_subnet.client.id
+ route_table_id = aws_route_table.igw.id
+}
+
+resource "aws_route_table_association" "router" {
+ subnet_id      = aws_subnet.router.id
+ route_table_id = aws_route_table.igw.id
+}
+
+resource "aws_route_table_association" "server" {
+ subnet_id      = aws_subnet.server.id
+ route_table_id = aws_route_table.igw.id
 }
 
 #
@@ -32,7 +90,7 @@ resource "aws_key_pair" "instance" {
 
 resource "aws_security_group" "instance" {
   name        = "udp-router-loadtest-sg"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   tags = {
     Terraform = "true"
@@ -42,7 +100,7 @@ resource "aws_security_group" "instance" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_local" {
   security_group_id = aws_security_group.instance.id
-  cidr_ipv4         = module.vpc.vpc_cidr_block
+  cidr_ipv4         = "10.0.0.0/16"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
@@ -70,14 +128,14 @@ module "clients" {
   count = var.num_client_instances
   name = "udp-router-loadtest-client-${count.index}"
 
-  private_ip             = cidrhost("10.0.0.0/24", 10 + count.index)
+  private_ip             = cidrhost("10.0.1.0/24", 10 + count.index)
   associate_public_ip_address = true
   ami                    = "ami-0929f698754f34ba7"
   instance_type          = "c7gn.xlarge"
   key_name               = "udp-router-loadtest-key"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.instance.id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = aws_subnet.client.id
 
   tags = {
     Terraform   = "true"
@@ -89,15 +147,15 @@ module "router" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   name = "udp-router-loadtest-router"
 
-  private_ip             = "10.0.0.100"
-  secondary_private_ips  = ["10.0.0.101"]
+  private_ip             = "10.0.2.10"
+  secondary_private_ips  = ["10.0.2.11"]
   associate_public_ip_address = true
   ami                    = "ami-0929f698754f34ba7"
   instance_type          = "c7gn.xlarge"
   key_name               = "udp-router-loadtest-key"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.instance.id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = aws_subnet.router.id
 
   tags = {
     Terraform   = "true"
@@ -111,14 +169,14 @@ module "servers" {
 
   name = "udp-router-loadtest-server-${count.index}"
 
-  private_ip             = cidrhost("10.0.0.0/24", 200 + count.index)
+  private_ip             = cidrhost("10.0.3.0/24", 10 + count.index)
   associate_public_ip_address = true
   ami                    = "ami-0929f698754f34ba7"
   instance_type          = "c7gn.xlarge"
   key_name               = "udp-router-loadtest-key"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.instance.id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = aws_subnet.server.id
 
   tags = {
     Terraform   = "true"
