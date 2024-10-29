a90b692acb4ae1ac555e23151fbc2e66bbec1383
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2024-10-29 11:03:38 +0200

    Implement reflection server

--- udp-router-server/src/main.rs
+++ udp-router-server/src/main.rs
@@ -1,40 +1,18 @@
 use tokio::net::UdpSocket;
-use std::error::Error;
-use std::net::Ipv4Addr;
-use std::str::FromStr;
-use clap::Parser;
-
-#[derive(Clone, Debug, Parser)]
-struct Opt {
-    /// IP address of UDP router
-    #[clap(long)]
-    proxy: String,
-    /// IP address of server to ping
-    #[clap(long)]
-    server: String,
-}
+use tokio::io;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let opt = Opt::parse();
+async fn main() -> io::Result<()> {
+    // Bind the socket to an address
+    let socket = UdpSocket::bind("0.0.0.0:8888").await?;
+    println!("Server listening on 0.0.0.0:8888");
 
-    // Bind to any available local port
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
+    let mut buf = [0u8; 1024];
 
-    // Define the message to be sent
-    let server_ip: u32 = Ipv4Addr::from_str(&opt.server).unwrap().into();
-    let payload = server_ip.to_be_bytes();
-
-    println!("payload is: {:?}", payload);
-
-    // Send the UDP packet
-    let bytes_sent = socket.send_to(&payload, format!("{}:8888", opt.proxy)).await?;
-    println!("Sent packet to {}", opt.proxy);
-
-    // Wait for response
-    let mut buffer = [0_u8; 1024];
-    let (bytes_received, src_addr) = socket.recv_from(&mut buffer).await?;
-
-    println!("Received response");
-    Ok(())
+    loop {
+        // Receive a message
+        let (len, addr) = socket.recv_from(&mut buf).await?;
+        let received = &buf[..len];
+        socket.send_to(received, &addr).await?;
+    }
 }
